e2dd10df8e26f7767756f2ae002a26ee46232cac
Abdo saf <noodsaf01@gmail.com>
2024-02-17 11:55:25 +0100

    first cooking

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import LeftMenu from './component/LeftMenu'
+import './App.css'
+
+function App() {
+
+
+return (
+  <>
+    
+
+
+
+<LeftMenu />
+
+
+
+
+      {/* <Swiper
+        spaceBetween={30}
+        hashNavigation={{
+          watchState: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, HashNavigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide data-hash="slide1">Slide 1</SwiperSlide>
+        <SwiperSlide data-hash="slide2">Slide 2</SwiperSlide>
+        <SwiperSlide data-hash="slide3">Slide 3</SwiperSlide>
+        <SwiperSlide data-hash="slide4">Slide 4</SwiperSlide>
+        <SwiperSlide data-hash="slide5">Slide 5</SwiperSlide>
+        <SwiperSlide data-hash="slide6">Slide 6</SwiperSlide>
+        <SwiperSlide data-hash="slide7">Slide 7</SwiperSlide>
+        <SwiperSlide data-hash="slide8">Slide 8</SwiperSlide>
+        <SwiperSlide data-hash="slide9">Slide 9</SwiperSlide>
+      </Swiper> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
