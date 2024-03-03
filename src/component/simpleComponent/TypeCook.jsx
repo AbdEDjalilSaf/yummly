740b64ae1843506b05eb8671338f8295560b8bae
Abdo saf <noodsaf01@gmail.com>
2024-03-03 17:36:51 +0100

    first cooking

--- src/component/simpleComponent/TypeCook.jsx
+++ src/component/simpleComponent/TypeCook.jsx
@@ -1,78 +1,44 @@
-import React from "react";
-import "./TypeCook.css"
+import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useState } from "react";
+
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { Pagination, Navigation, HashNavigation } from 'swiper/modules';
+import 'swiper/css/pagination';
 
+import './TypeCook.css';
 
+// import required modules
+import { Navigation, Pagination, History } from 'swiper/modules';
 
+export default function App() {
 
-
-
-function TypeCook(){
-    const [activeIndex, setActiveIndex] = useState(0);
     const buttons = ['Button 1', 'Button 2', 'Button 3', 'Button 4'];
-  
-const handleButtonClick = (index) => {
-    setActiveIndex(index);
-};
 
-
-return (
-<>
-<div className="flex justify-center">
-<Swiper
-      spaceBetween={1}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      className="w-[300px] h-[800px]"
-    >
-
-{buttons.map((souds, index) => (
-          <SwiperSlide key={index}>
-<button className={`border-none outline-none cursor-pointer bg-[#000] text-[#333] rounded-full w-[60px] h-[60px] mx-[10px]  ${index === activeIndex ? 'border-none outline-none cursor-pointer rounded-full w-[60px] h-[60px]  bg-[#007bff] text-[#fff] mx-[10px] ' : ''}`} onClick={()=> handleButtonClick()}>{souds}</button>
-          </SwiperSlide>
-
-        ))}
-   
-
-
-</Swiper>
- 
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</>
-)
-
-
+  return (
+    <>
+    
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={4}
+        navigation={true}
+        history={{
+          key: 'slide',
+        }}
+        modules={[Navigation, Pagination, History]}
+        className="mySwiper"
+      >
+        <SwiperSlide data-history="1">Slide 1</SwiperSlide>
+        <SwiperSlide data-history="Slide 2">Slide 2</SwiperSlide>
+        <SwiperSlide data-history="3">Slide 3</SwiperSlide>
+        <SwiperSlide data-history="Slide 4">Slide 4</SwiperSlide>
+        <SwiperSlide data-history="5">Slide 5</SwiperSlide>
+        <SwiperSlide data-history="Slide 6">Slide 6</SwiperSlide>
+        <SwiperSlide data-history="7">Slide 7</SwiperSlide>
+        <SwiperSlide data-history="Slide 8">Slide 8</SwiperSlide>
+        <SwiperSlide data-history="9">Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
-
-
-export default TypeCook;
